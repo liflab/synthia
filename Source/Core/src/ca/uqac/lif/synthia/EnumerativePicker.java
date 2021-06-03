09fc328b6fd9ba63dd65ccbdbffdf3646a507af9
Marc-Antoine Plourde <56414802+map555@users.noreply.github.com>
2021-06-03 13:22:39 -0400

    Update EnumerativePicker.java

--- Source/Core/src/ca/uqac/lif/synthia/EnumerativePicker.java
+++ Source/Core/src/ca/uqac/lif/synthia/EnumerativePicker.java
@@ -6,7 +6,7 @@ import ca.uqac.lif.synthia.Picker;
  * Interface who extends Picker. This interface will be used to signal
  * that the picker enumerates all values from a set.
  */
-public interface EnumerativePicker extends Picker
+public interface EnumerativePicker<T> extends Picker<T>
 {
 	/**
 	 * Signals if the picker enumerates all values from a set.
