9ed4672f822d7035495f175a88880255f926192a
marc <marc-antoine.plourde1@uqac.ca>
2021-07-05 13:42:23 -0400

    close #22

--- Source/CoreTest/src/ca/uqac/lif/synthia/enumerative/AllBooleansTest.java
+++ Source/CoreTest/src/ca/uqac/lif/synthia/enumerative/AllBooleansTest.java
@@ -2,6 +2,7 @@ package ca.uqac.lif.synthia.enumerative;
 
 import ca.uqac.lif.synthia.NoMoreElementException;
 import ca.uqac.lif.synthia.Picker;
+import ca.uqac.lif.synthia.random.RandomInteger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -73,6 +74,82 @@ public class AllBooleansTest
 	public void duplicateWithoutState()
 	{
 		AllBooleans all_bool =  new AllBooleans();
+		all_bool.pick();
+
+		AllBooleans all_bool_copy = (AllBooleans) all_bool.duplicate(false);
+
+		Assertions.assertNotEquals(all_bool.pick(), all_bool_copy.pick());
+
+		all_bool.reset();
+		all_bool_copy.reset();
+
+		Assertions.assertEquals(all_bool.pick(), all_bool_copy.pick());
+		Assertions.assertEquals(all_bool.pick(), all_bool_copy.pick());
+	}
+
+	@Test
+	public void scrambleFalse()
+	{
+		AllBooleans all_bool =  new AllBooleans(false);
+
+		Assertions.assertEquals(false, all_bool.pick());
+		Assertions.assertEquals(true, all_bool.pick());
+	}
+
+	@Test
+	public void scrambleTrue()
+	{
+		AllBooleans all_bool = new AllBooleans(true);
+
+		while(!all_bool.isDone())
+		{
+			System.out.println(all_bool.pick());
+		}
+	}
+
+
+	@Test
+	public void noMoreExceptionScramble()
+	{
+		AllBooleans all_bool =  new AllBooleans(true);
+
+		for (int i = 0; i <2; i++)
+		{
+			all_bool.pick();
+		}
+
+		noMoreExceptionThrow(all_bool);
+	}
+
+	@Test
+	public void isDoneScramble()
+	{
+		AllBooleans all_bool =  new AllBooleans();
+
+		for (int i = 0; i < 2; i++)
+		{
+			Assertions.assertEquals(false, all_bool.isDone());
+			all_bool.pick();
+		}
+
+		Assertions.assertEquals(true, all_bool.isDone());
+	}
+
+	@Test
+	public void duplicateWithStateScramble()
+	{
+		AllBooleans all_bool =  new AllBooleans(true);
+		all_bool.pick();
+
+		AllBooleans all_bool_copy = (AllBooleans) all_bool.duplicate(true);
+
+		Assertions.assertEquals(all_bool.pick(), all_bool_copy.pick());
+	}
+
+	@Test
+	public void duplicateWithoutStateScramble()
+	{
+		AllBooleans all_bool =  new AllBooleans(true);
 		all_bool.pick();
 
 		AllBooleans all_bool_copy = (AllBooleans) all_bool.duplicate(false);
--- Source/CoreTest/src/ca/uqac/lif/synthia/enumerative/AllIntegersTest.java
+++ Source/CoreTest/src/ca/uqac/lif/synthia/enumerative/AllIntegersTest.java
@@ -2,6 +2,7 @@ package ca.uqac.lif.synthia.enumerative;
 
 import ca.uqac.lif.synthia.NoMoreElementException;
 
+import ca.uqac.lif.synthia.random.RandomInteger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -92,6 +93,102 @@ public class AllIntegersTest
 		int min = 0;
 		int max = 10;
 		AllIntegers all_int =  new AllIntegers(min, max);
+
+		for (int i = 0; i <= (max/2); i++)
+		{
+			all_int.pick();
+		}
+
+		AllIntegers all_int_copy = (AllIntegers) all_int.duplicate(false);
+
+		for (int i = 0; i < (max/2); i++)
+		{
+			Assertions.assertNotEquals(all_int.pick().intValue(), all_int_copy.pick().intValue());
+		}
+
+		all_int.reset();
+		all_int_copy.reset();
+
+		for (int i = 0; i <= max; i++)
+		{
+			Assertions.assertEquals(all_int.pick().intValue(), all_int_copy.pick().intValue());
+		}
+	}
+
+	@Test
+	public void scramble()
+	{
+		int min = 0;
+		int max = 10;
+		AllIntegers all_int =  new AllIntegers(min, max,true);
+
+		while (!all_int.isDone())
+		{
+			System.out.println(all_int.pick());
+		}
+	}
+
+	@Test
+	public void noMoreExceptionScramble()
+	{
+		int min = 0;
+		int max = 10;
+		AllIntegers all_int =  new AllIntegers(min, max,true);
+
+		for (int i = 0; i <= max; i++)
+		{
+			all_int.pick();
+		}
+
+		noMoreExceptionThrow(all_int);
+	}
+
+	@Test
+	public void isDoneScramble()
+	{
+		int min = 0;
+		int max = 10;
+		AllIntegers all_int =  new AllIntegers(min, max, true);
+
+		for (int i = 0; i <= max; i++)
+		{
+			Assertions.assertEquals(false, all_int.isDone());
+			all_int.pick();
+		}
+
+		Assertions.assertEquals(true, all_int.isDone());
+	}
+
+	@Test
+	public void duplicateWithStateScramble()
+	{
+		int min = 0;
+		int max = 10;
+		RandomInteger seed = new RandomInteger(0, 100000);
+		AllIntegers all_int =  new AllIntegers(min, max, true);
+		all_int.setSeed(seed.pick());
+
+		for (int i = 0; i <= (max/2); i++)
+		{
+			all_int.pick();
+		}
+
+		AllIntegers all_int_copy = (AllIntegers) all_int.duplicate(true);
+
+		for (int i = 0; i < (max/2); i++)
+		{
+			Assertions.assertEquals(all_int.pick().intValue(), all_int_copy.pick().intValue());
+		}
+	}
+
+	@Test
+	public void duplicateWithoutStateScramble()
+	{
+		int min = 0;
+		int max = 10;
+		RandomInteger seed = new RandomInteger(0, 100000);
+		AllIntegers all_int =  new AllIntegers(min, max, true);
+		all_int.setSeed(seed.pick());
 
 		for (int i = 0; i <= (max/2); i++)
 		{
