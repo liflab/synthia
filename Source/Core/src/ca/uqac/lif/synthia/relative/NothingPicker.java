a08645b941d7ab194d6aedbedbdeb10dc37af832
marc <marc-antoine.plourde1@uqac.ca>
2021-07-15 15:36:23 -0400

    close #29

---
+++ Source/Core/src/ca/uqac/lif/synthia/relative/NothingPicker.java
@@ -0,0 +1,45 @@
+package ca.uqac.lif.synthia.relative;
+
+import ca.uqac.lif.synthia.Picker;
+import ca.uqac.lif.synthia.exception.NoMoreElementException;
+
+/**
+ * A {@link ca.uqac.lif.synthia.relative.RelativePicker} who only throw a
+ * {@link ca.uqac.lif.synthia.exception.NoMoreElementException} when the {@link #pick()} method is
+ * called.
+ *
+ * @param <T> The type of the picker(but not really important here).
+ */
+public class NothingPicker<T> implements Picker, RelativePicker
+{
+	public NothingPicker(){};
+
+	/**
+	 * Because this picker only return throws a
+	 * {@link ca.uqac.lif.synthia.exception.NoMoreElementException}, the following method only returns
+	 * a new instance of the class.
+	 */
+	@Override
+	public RelativePicker getPicker(Object element)
+	{
+		return duplicate(true);
+	}
+
+	@Override
+	public void reset()
+	{
+		//Nothing to do here.
+	}
+
+	@Override
+	public Object pick()
+	{
+		throw  new NoMoreElementException();
+	}
+
+	@Override
+	public RelativePicker duplicate(boolean with_state)
+	{
+		return new NothingPicker();
+	}
+}
---
+++ Source/Core/srctest/src/ca/uqac/lif/synthia/relative/NothingPickerTest.java
@@ -0,0 +1,36 @@
+package ca.uqac.lif.synthia.relative;
+
+import ca.uqac.lif.synthia.exception.NoMoreElementException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NothingPickerTest
+{
+	private void noMoreElementThrow(NothingPicker picker)
+	{assertThrows(NoMoreElementException.class, new Executable()
+		{
+			@Override
+			public void execute() throws Throwable
+			{
+				picker.pick();
+			}
+		});
+	}
+
+	@Test
+	public void pick()
+	{
+		NothingPicker picker = new NothingPicker();
+		noMoreElementThrow(picker);
+	}
+
+	@Test
+	public void getPicker()
+	{
+		int element = 2;
+		NothingPicker picker = new NothingPicker();
+		noMoreElementThrow((NothingPicker) picker.getPicker(element));
+	}
+}
