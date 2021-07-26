872fc3991f4f0073d10fc32f667e3812a56f7609
marc <marc-antoine.plourde1@uqac.ca>
2021-07-26 16:55:32 -0400

    close #33

---
+++ Source/Core/src/ca/uqac/lif/synthia/random/RandomSuffix.java
@@ -0,0 +1,45 @@
+package ca.uqac.lif.synthia.random;
+
+import ca.uqac.lif.synthia.relative.NothingPicker;
+import ca.uqac.lif.synthia.relative.RelativePicker;
+
+/**
+ * Just like {@link RandomPrefix} but for suffixes.
+ */
+public class RandomSuffix extends RandomPrefix
+{
+
+	public RandomSuffix(String string)
+	{
+		super(string);
+	}
+
+	private RandomSuffix(String string, RandomInteger prefix_size)
+	{
+		super(string, prefix_size);
+	}
+
+	@Override
+	public RelativePicker<String> getPicker(String element)
+	{
+		if(element.isEmpty())
+		{
+			return new NothingPicker<String>();
+		}
+		return new RandomSuffix(element, m_prefixSize.duplicate(true));
+	}
+
+	@Override
+	public String pick()
+	{
+		return m_string.substring(m_prefixSize.pick(), m_string.length());
+	}
+
+	@Override
+	public RandomSuffix duplicate(boolean with_state)
+	{
+		return new RandomSuffix(m_string, m_prefixSize.duplicate(with_state));
+	}
+
+
+}
