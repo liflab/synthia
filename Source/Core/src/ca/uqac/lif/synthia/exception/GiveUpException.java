abe6ab2054b4842aa7e86d4d839d4f5b4885bddd
marc <marc-antoine.plourde1@uqac.ca>
2021-08-13 10:50:52 -0400

    Update GiveUpException.java

--- Source/Core/src/ca/uqac/lif/synthia/exception/GiveUpException.java
+++ Source/Core/src/ca/uqac/lif/synthia/exception/GiveUpException.java
@@ -1,7 +1,9 @@
 package ca.uqac.lif.synthia.exception;
 
+import ca.uqac.lif.synthia.relative.PickIf;
+
 /**
- * An exception to throw to prevent the pick method of a {@link ca.uqac.lif.synthia.PickIf} picker
+ * An exception to throw to prevent the pick method of a {@link PickIf} picker
  * from falling into an infinite loop.
  */
 public class GiveUpException extends PickerException
