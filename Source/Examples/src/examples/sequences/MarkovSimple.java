f585f24b477131e911f59009bfb3da68bccf4e2e
Sylvain Hallé <shalle@acm.org>
2021-11-01 15:35:17 -0400

    Improved Javadoc

--- Source/Examples/src/examples/sequences/MarkovSimple.java
+++ Source/Examples/src/examples/sequences/MarkovSimple.java
@@ -1,7 +1,22 @@
-/**
- * @ingroup Examples
+/*
+    Synthia, a data structure generator
+    Copyright (C) 2019-2021 Laboratoire d'informatique formelle
+    Université du Québec à Chicoutimi, Canada
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package examples.basic;
+package examples.sequences;
 
 import ca.uqac.lif.synthia.random.RandomFloat;
 import ca.uqac.lif.synthia.sequence.MarkovChain;
@@ -10,8 +25,12 @@ import ca.uqac.lif.synthia.string.StringPattern;
 import ca.uqac.lif.synthia.util.Constant;
 import ca.uqac.lif.synthia.util.Tick;
 
-public class MarkovSimple {
-
+/**
+ * Generates a sequence of values according to a Markov chain.
+ * @ingroup Examples
+ */
+public class MarkovSimple
+{
 	public static void main(String[] args)
 	{
 		Tick tick = new Tick(0, new Playback<Integer>(new Integer[] {1, 2, 1}));
