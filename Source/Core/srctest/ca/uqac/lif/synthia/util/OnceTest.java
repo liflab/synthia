a1235d25657e055d90f06d72693e3e0d77ce7d12
Sylvain Hallé <shalle@acm.org>
2024-04-17 16:29:36 -0400

    Warning fixes to unit tests in Once

--- Source/Core/srctest/ca/uqac/lif/synthia/util/OnceTest.java
+++ Source/Core/srctest/ca/uqac/lif/synthia/util/OnceTest.java
@@ -1,3 +1,21 @@
+/*
+    Synthia, a data structure generator
+    Copyright (C) 2019-2024 Laboratoire d'informatique formelle
+    Université du Québec à Chicoutimi, Canada
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ca.uqac.lif.synthia.util;
 
 import ca.uqac.lif.synthia.NoMoreElementException;
@@ -8,12 +26,15 @@ import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+/**
+ * Unit tests for the {@link Once} class.
+ */
 public class OnceTest
 {
 	//Returns AssertionFailedError if NoMoreElementException is not thrown.
-	public void noMoreExceptionThrow(Once once)
+	public void noMoreExceptionThrow(Once<?> once)
 	{
-		NoMoreElementException exception = assertThrows(NoMoreElementException.class, new Executable()
+		assertThrows(NoMoreElementException.class, new Executable()
 		{
 			@Override
 			public void execute() throws Throwable
@@ -27,7 +48,7 @@ public class OnceTest
 	public void noMoreExceptionTest()
 	{
 		RandomFloat randomFloat = new RandomFloat();
-		Once once = new Once(randomFloat);
+		Once<Float> once = new Once<>(randomFloat);
 		once.pick();
 		noMoreExceptionThrow(once);
 	}
@@ -36,9 +57,9 @@ public class OnceTest
 	public void duplicateWithState()
 	{
 		RandomFloat randomFloat = new RandomFloat();
-		Once once = new Once(randomFloat);
+		Once<Float> once = new Once<>(randomFloat);
 		once.pick();
-		Once once_copy = once.duplicate(true);
+		Once<Float> once_copy = once.duplicate(true);
 		noMoreExceptionThrow(once);
 		noMoreExceptionThrow(once_copy);
 	}
@@ -47,9 +68,9 @@ public class OnceTest
 	public void duplicateWhitoutState()
 	{
 		RandomFloat randomFloat = new RandomFloat();
-		Once once = new Once(randomFloat);
+		Once<Float> once = new Once<>(randomFloat);
 		float original_value = (float) once.pick();
-		Once once_copy = once.duplicate(false);
+		Once<Float> once_copy = once.duplicate(false);
 		float copy_value = (float) once_copy.pick();
 		Assertions.assertEquals(original_value, copy_value);
 		noMoreExceptionThrow(once);
