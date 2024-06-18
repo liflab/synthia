3230a798b57c768e90e3fcfaa29e84a4311ea7b7
Sylvain Hallé <shalle@acm.org>
2024-06-18 16:03:53 -0400

    Start of BPMN extension

---
+++ Source/Bpmn/src/ca/uqac/lif/synthia/bpmn/BpmnElement.java
@@ -0,0 +1,13 @@
+package ca.uqac.lif.synthia.bpmn;
+
+import ca.uqac.lif.dag.Node;
+
+public class BpmnElement extends Node
+{
+
+	public BpmnElement(int in_arity, int out_arity)
+	{
+		super(in_arity, out_arity);
+	}
+
+}
---
+++ Source/Bpmn/src/ca/uqac/lif/synthia/bpmn/package-info.java
@@ -0,0 +1,1 @@
+package ca.uqac.lif.synthia.bpmn;
